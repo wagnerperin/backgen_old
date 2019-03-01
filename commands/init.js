8f43e7797e3d903f1dc2b7a89797836f84e2611d
Wagner de Andrade Perin <wagnerperin@gmail.com>
2019-03-01 18:28:37 -0300

    More improves in project organization

--- commands/init.js
+++ commands/init.js
@@ -22,14 +22,14 @@ module.exports = () => {
                     dependencies: dependencies(answers.backgen.databaseType),
                     devDependencies: devDependencies(answers.backgen.databaseType)
                 }
-
+                
                 if(argv.dir !== ".") 
                     if(fs.existsSync(path.join(process.cwd(), argv.dir))) throw new Error('Can\'t create this project, this folder name already exists.');
                     else { 
-                        fs.mkdirSync(path.join(process.cwd(), argv.dir), {recursive: true});
+                        fs.mkdirSync(path.join(process.cwd(), argv.dir), { recursive: true });
                         process.chdir(path.join(process.cwd(), argv.dir))
                     }
-                
+                fs.writeFileSync(path.join(process.cwd(), 'package.json'), JSON.stringify(answers, null, 2));
                 ncp(path.join(require.main.filename,'../references'), './', (err) => {
                     if(err) console.log(err);
                 })
--- references/config/express.js
+++ references/config/express.js
@@ -1,6 +1,19 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
 const consign = require('consign');
+const apiRoutes = require('./apiRoutes.json');
 
+for(r in apiRoutes) app.set(r, apiRoutes[r]);
+
+app.use(bodyParser.json());
+
+consign({ cwd : 'app' })
+    .include('helpers')
+    .then('errors')
+    .then('models')
+    .then('api')
+    .then('routes')
+    .then('swagger')
+    .into(app);
 
 module.exports = app;
