a7532e245d6eeb81533791d66f2c20fa3ab5c84d
Wagner de Andrade Perin <wagnerperin@gmail.com>
2019-03-09 12:05:34 -0300

    WIP_Implementing handlebars

--- commands/init.js
+++ commands/init.js
@@ -7,6 +7,7 @@ module.exports = () => {
     const dependencies = require('../core/dependencies');
     const devDependencies = require('../core/devDependencies');
     const ncp = require('ncp').ncp;
+    const handlebars = require('handlebars');
     
     const command = argv._[0];
 
@@ -32,6 +33,31 @@ module.exports = () => {
                 fs.writeFileSync(path.join(process.cwd(), 'package.json'), JSON.stringify(answers, null, 2));
                 ncp(path.join(require.main.filename,'../references'), './', (err) => {
                     if(err) console.log(err);
+                    else if(answers.backgen.security.JWTAuthentication){
+                        fs.readFile(path.join(require.main.filename,'../templates/route.hbs'), 'utf8', (err, routeTemplate) => {
+                            if(err) console.log(err);
+                            else {
+                                let template = handlebars.compile(routeTemplate);
+                                let userRoute = {
+                                    name: 'user',
+                                    authenticationRequired: false
+                                }
+                                fs.writeFile(path.join(process.cwd(), '/app/routes/user.js'), template(userRoute), (err) => {
+                                    if(err) console.log(err)
+                                });
+                                let authRoute = {
+                                    name: 'auth',
+                                    authenticationRequired: false,
+                                    create: {
+                                        method: 'authenticate'
+                                    }
+                                }
+                                fs.writeFile(path.join(process.cwd(), '/app/routes/auth.js'), template(authRoute), (err) => {
+                                    if(err) console.log(err)
+                                });
+                            }
+                        })
+                    }
                 })
             });
 
--- core/questions.js
+++ core/questions.js
@@ -45,6 +45,31 @@ let questions = {
             name: 'backgen.databaseType',
             message: 'Which database do you plan to use?',
             choices: ['MongoDB', 'MySQL']
+        },
+        {
+            type: 'confirm',
+            name: 'backgen.security.JWTAuthentication',
+            message: 'Do you want to include a basic JWT Authentication?'
+        },
+        {
+            type: 'checkbox',
+            name: 'backgen.routes.user.methods',
+            message: 'Select the user api methods that you want to generate',
+            choices: ['create', 'list', 'retrieve', 'update', 'delete'],
+            when: function(answers) {
+                return answers.backgen.security.JWTAuthentication;
+            }
+        },
+        {
+            type: 'checkbox',
+            name: 'backgen.routes.user.restrictedMethods',
+            message: 'Select the user api methods that you want to protect with authencationRequired restriction',
+            choices: function(answers) {
+                return answers.backgen.routes.user.methods;
+            },
+            when: function(answers) {
+                return answers.backgen.security.JWTAuthentication;
+            }
         }
     ]
 }
